5a6f31a05ad46e174c5793d8f424b75fddde457f
Victor Moreno <94730998+VictorabMoreno@users.noreply.github.com>
2022-06-14 11:42:03 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        blackttt: '#15202B',
+        blacktt: '#000000',
+        bluett: '#1D9BF0',
+        onix:'#333639',
+        pratatt: '#71767B',
+        platinatt: '#E7E9EA',
+        red: 'red'
+      }
+    },
+  },
+  plugins: [],
+}
